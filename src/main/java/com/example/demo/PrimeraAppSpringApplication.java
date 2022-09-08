07579c66813d81ce4b12ed2f2fab509b50d32800
Alexis <raulnoboa@gmail.com>
2022-09-08 17:31:04 -0500

    Primer Commit Eclipse

---
+++ src/main/java/com/example/demo/PrimeraAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraAppSpringApplication.class, args);
+	}
+
+}
